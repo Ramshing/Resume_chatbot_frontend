7c5a15f26f573d4d909ef98a329692a5487e224a
Ram <dnshram013@gmail.com>
2026-05-14 15:42:01 +0530

    update vite build changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 console.log(import.meta.env.VITE_API_BASE_URL)
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
